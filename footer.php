35498ae4fbac1005c3cae1b1512ad027852fcbe2
unknown <samvthom16@gmail.com>
2020-07-27 21:20:28 +0530

    #1 - integrate google cse

--- footer.php
+++ footer.php
@@ -35,14 +35,15 @@
             );
         ?>
 				<div class="social col-sm-offset-2 col-sm-3">
-				<form class="navbar-form clearfix" role="search">  
+				<form class="navbar-form clearfix" role="search">
 					<span class="social-icons">
 						<a href="https://www.facebook.com/HNFP.IN/"><i class="fa fa-facebook fa-outline"></i></a>
 						<a href="https://twitter.com/hnfp_in"><i class="fa fa-twitter fa-outline"></i></a>
 						<a href="whatsapp://send?text=<?php bloginfo('url');?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp"></i></a>
 					</span>
 					<a href=""><i class="fa fa-search"></i></a>
-					<input type="text" name="s" class="search-query" placeholder="Search" />
+					<?php _e( do_shortcode( '[wp_google_searchbox]' ) );?>
+					<!--input type="text" name="s" class="search-query" placeholder="Search" /-->
 				</form>
 				</div>
 			</div>
@@ -52,3 +53,24 @@
 <?php wp_footer();?>
 </body>
 </html>
+<style>
+	.wgs_wrapper{
+		display: inline-block;
+		vertical-align: middle;
+		width: 70px;
+	}
+	.wgs_wrapper .gsc-control-cse{ padding: 0;}
+	.wgs_wrapper .gsc-input-box, .wgs_wrapper .gsc-input-box .gsc-input{
+		background: transparent !important;
+		font: lighter medium/2 "Montserrat" !important;
+	}
+	.wgs_wrapper .gsc-search-button{ display: none; }
+	.wgs_wrapper .gsc-input-box td{ padding: 0 !important; }
+	.wgs_wrapper .gsib_b{ display: none; }
+
+</style>
+​<script>
+  window.onload = function(){
+		jQuery( '.gsc-input' ).attr( 'placeholder', 'Search' );
+  };
+</script>
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></title>
-	
+
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-102742282-2"></script>
 <script>
@@ -15,32 +15,33 @@
 
   gtag('config', 'UA-102742282-2');
 </script>
-	
+
 <?php wp_head();?>
 </head>
 <body <?php body_class(); ?> >
 <div id="fb-root"></div>
 <nav class="navbar navbar-default no-border" data-spy="affix" data-offset-top="90">
-  <div class="container-fluid line"> 
+  <div class="container-fluid line">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <a class="navbar-brand-big" href="<?php bloginfo('url');?>"> <img src="<?php echo get_theme_mod( 'm1_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
       </a>
-      <a class="navbar-brand hidden" href="<?php bloginfo('url');?>"> <img src="<?php bloginfo('template_directory'); ?>/images/logo_sticky.png" alt=""> </a> 
+      <a class="navbar-brand hidden" href="<?php bloginfo('url');?>"> <img src="<?php bloginfo('template_directory'); ?>/images/logo_sticky.png" alt=""> </a>
 	  </div>
     <div class="navbar-right">
 	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><i class="fa fa-bars"></i> </button>
-        <form class="navbar-form clearfix" role="search">       
+        <form class="navbar-form clearfix" role="search">
 				<div class="pull-right social red-text">
 					<span class="social-icons">
 						<a href="https://www.facebook.com/HNFP.IN/"><i class="fa fa-facebook fa-outline"></i></a>
 						<a href="https://twitter.com/hnfp_in"><i class="fa fa-twitter fa-outline"></i></a>
-						<a href="whatsapp://send?text=<?php bloginfo('url');?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp"></i></a> 
+						<a href="whatsapp://send?text=<?php bloginfo('url');?>" data-action="share/whatsapp/share"><i class="fa fa-whatsapp"></i></a>
 					</span>
 					<a href=""><i class="fa fa-search"></i></a>
-					<input type="text" name="s" class="search-query" placeholder="Search" />
+					<?php _e( do_shortcode( '[wp_google_searchbox]' ) );?>
+					<!--input type="text" name="s" class="search-query" placeholder="Search" /-->
 					<span class="hidden">
-						<span class="separator text-muted">Next Story</span> 
+						<span class="separator text-muted">Next Story</span>
 						<?php $next = get_the_title(get_adjacent_post(false,'',true));
 						$next_link = get_permalink(get_adjacent_post(false,'',true));?>
 						<a href="<?php echo $next_link ?>" class="next-post text-muted"><strong><?php echo $next ?></strong></a>
@@ -55,15 +56,15 @@
                 'depth'             => 2,
                 'container'         => 'div',
                 'container_class'   => 'collapse navbar-collapse text-uppercase col-md-12 pull-left',
-				'container_id'      => 'bs-example-navbar-collapse-1',
+								'container_id'      => 'bs-example-navbar-collapse-1',
                 'menu_class'        => 'nav navbar-nav navbar-right',
                 'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                 'walker'            => new wp_bootstrap_navwalker())
             );
         ?>
 	</div>
-    <!-- /.navbar-collapse --> 
+    <!-- /.navbar-collapse -->
   </div>
-  <!-- /.container-fluid --> 
+  <!-- /.container-fluid -->
 </nav>
 <?php get_template_part( 'header', 'banner' ); ?>
